8b3f0c3c3b0353c739b8f2b868eafc2a57d08f26
jjy596900 <twojsm05@gmail.com>
2025-04-14 16:02:53 +0900

    made Enemy

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private bool isMovingRight = true;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isMovingRight)
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        else 
+            rb.velocity = new Vector2(moveSpeed, -rb.velocity.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Boundary")) 
+        {
+            isMovingRight = !isMovingRight;
+        }
+    }
+
+
+
+}
