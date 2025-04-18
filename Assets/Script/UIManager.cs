1d0d6f331def4514a93804bd954229c073d60538
jjy596900 <twojsm05@gmail.com>
2025-04-19 02:08:04 +0900

    [Feat]made stage, Exit Button

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -22,5 +22,13 @@ public class UIManager : MonoBehaviour
         HelpPanel.SetActive(false);
     }
 
+    public void BtnExit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 
 }
